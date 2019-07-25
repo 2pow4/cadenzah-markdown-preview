3654983c25f730c07b3fef3259ec6c97c6e1dbea
cadenzah <cadenzah93@gmail.com>
2019-07-25 22:21:16 +0900

    - added few words in README.md - added styles

--- src/jsx/Editor.jsx
+++ src/jsx/Editor.jsx
@@ -3,6 +3,7 @@ const React = require('react')
 const Editor = (props) => (
   <div className="editor">
     <textarea
+      placeholder="마크다운을 작성해보세요"
       autoFocus={false}
       onChange={(e) => props.onInputChange(e)}
       value={props.editorValue}
